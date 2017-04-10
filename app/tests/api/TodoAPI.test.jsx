f5f27b750b9b7869c8ef733037e8c92bb662276f
susuaung <susuaung.ms@gmail.com>
2017-04-10 21:55:22 +0900

    Add filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -52,4 +52,51 @@ describe('TodoAPI', () => {
       expect(actualTodos).toEqual(todos);
     });
   });
+
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: 'Some task here',
+      completed: true
+    }, {
+      id: 2,
+      text: 'Other task here',
+      completed: false
+    }, {
+      id: 3,
+      text: 'Some task here',
+      completed: true
+    }];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return uncompleted items if showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status ', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('shoud filter todos by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'other');
+
+      expect(filteredTodos.length).toBe(1);
+
+    });
+
+    it('should return all todos if searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+  });
 });
