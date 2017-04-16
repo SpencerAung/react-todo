a6a29ec090dad0b8afb3625baa115c9b71fdd671
susuaung <susuaung.ms@gmail.com>
2017-04-16 14:34:02 +0900

    Update todo on firebase

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -46,23 +46,28 @@ describe('Reducers', () => {
       expect(res[0]).toEqual(action.todo);
     });
 
-    it('should toggle todo', () => {
+    it('should update todo', () => {
       var todos = [{
         id: uuid(),
         text: 'Go to Yanje',
-        completed: false,
+        completed: true,
         createdAt: 123
       }];
-
+      var updates = {
+        completed: false,
+        completedAt: null
+      };
       var action = {
-        type: 'TOGGLE_TODO',
-        id: todos[0].id
-      }
+        type: 'UPDATE_TODO',
+        id: todos[0].id,
+        updates
+      };
 
       var res = reducers.todosReducer(df(todos), df(action));
 
-      expect(res[0].completed).toEqual(true);
-      expect(res[0].completedAt).toBeA('number');
+      expect(res[0].completed).toEqual(updates.completed);
+      expect(res[0].completedAt).toEqual(updates.completedAt);
+      expect(res[0].text).toEqual(todos[0].text);
     });
 
     it('should add existing todos', () => {
