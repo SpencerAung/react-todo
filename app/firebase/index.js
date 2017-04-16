ff030d64c301b2cf519b7ca598243ea42eb4db11
susuaung <susuaung.ms@gmail.com>
2017-04-16 17:02:21 +0900

    Move firebase creds to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 try {
   var config = {
-      apiKey: "AIzaSyDs2x3dG9MFMtqyvVrbT8Y--Qlz59Xm040",
-      authDomain: "todo-app-cf5dd.firebaseapp.com",
-      databaseURL: "https://todo-app-cf5dd.firebaseio.com",
-      projectId: "todo-app-cf5dd",
-      storageBucket: "todo-app-cf5dd.appspot.com",
-      messagingSenderId: "63608962667"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
 
   firebase.initializeApp(config);
